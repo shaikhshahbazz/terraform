a76bc66cee38572c6cbc2955ed3e852bff16acfc
Shaikh Shahbaz <theshaikhshahbazz@gmail.com>
2026-01-20 17:56:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
-resource "local_file" "my_pet" {
-  filename = var.filename
-  content  = var.content
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_iam_user" "admin_user" {
+name = "shahbazz"
+
+tags = {
+description = "Technical Team Lead"
+}
+}
+
+resource "aws_iam_user_policy_attachment" "shahbazz_s3_full_access" {
+user = aws_iam_user.admin_user.name
+policy_arn = "arn:aws:iam:aws:policy/AmazonS3FullAccess"
 }
 
